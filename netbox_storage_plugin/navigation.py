c80f70741f00b1c430c37f238881f216ff0721f3
Eric Hester <eric.hester@umbrella.tech>
2025-03-01 19:40:43 -0500

    fixed menu definition

--- netbox_storage_plugin/navigation.py
+++ netbox_storage_plugin/navigation.py
@@ -1,28 +1,28 @@
 from netbox.plugins import PluginMenu, PluginMenuItem, PluginMenuButton
 
+items = (
+    PluginMenuItem(
+        link='plugins:netbox_storage_plugin:volume_list',  
+        link_text='Volumes',  
+        buttons=(
+            PluginMenuButton(
+                link='plugins:netbox_storage_plugin:volume_add',  
+                title='Add',  
+                icon_class='mdi mdi-plus-thick',   
+                permissions=['netbox_storage_plugin.add_volume']  
+            ),
+            PluginMenuButton(
+                link='plugins:netbox_storage_plugin:volume_import',
+                title='Import',  
+                icon_class='mdi mdi-upload',  
+                permissions=['netbox_storage_plugin.import_volume']  
+            ),
+        )
+    ),
+)
+
 menu = PluginMenu(
-    label='Storage', 
-    icon_class='mdi mdi-harddisk', 
-    items=(
-        PluginMenuItem(
-            link='plugins:netbox_storage_plugin:volume_list',  
-            link_text='Volumes',  
-            buttons=(
-                PluginMenuButton(
-                    link='plugins:netbox_storage_plugin:volume_add',  
-                    title='Add',  
-                    icon_class='mdi mdi-plus-thick',  
-                    color='green',  
-                    permissions=['netbox_storage_plugin.add_volume']  
-                ),
-                PluginMenuButton(
-                    link='plugins:netbox_storage_plugin:volume_import',
-                    title='Import',  
-                    icon_class='mdi mdi-upload',  
-                    color='blue',  
-                    permissions=['netbox_storage_plugin.import_volume']  
-                ),
-            )
-        ),
-    )
+    label='Storage',
+    icon_class='mdi mdi-harddisk',
+    groups=(('Volumes', items),)
 )
