5b3709ac097c878afc87ceddc7bcc069d4176526
Eric Hester <eric.hester@umbrella.tech>
2025-03-01 20:11:00 -0500

    .

--- netbox_storage_plugin/tables.py
+++ netbox_storage_plugin/tables.py
@@ -1,11 +1,13 @@
 from netbox.tables import NetBoxTable, columns
+import django_tables2 as tables
+
 from .models import Volume
 
 class VolumeTable(NetBoxTable):
-    name = columns.Column(linkify=True)
-    type = columns.Column()
-    size = columns.Column()
-    description = columns.Column()
+    name = tables.Column(linkify=True)
+    type = tables.Column()
+    size = tables.Column()
+    description = tables.Column()
 
     class Meta(NetBoxTable.Meta):
         model = Volume
