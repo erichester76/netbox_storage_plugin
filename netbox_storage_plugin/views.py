df8b43c3fba431bcff40e387edeaa1139bbb7604
Eric Hester <eric.hester@umbrella.tech>
2025-03-02 13:17:23 -0500

    .

--- netbox_storage_plugin/views.py
+++ netbox_storage_plugin/views.py
@@ -2,82 +2,66 @@ from netbox.views import generic
 from . import models, forms, tables
 
 class DiskListView(generic.ObjectListView):
-    template_name = 'netbox_storage_plugin/storage_list.html'
     queryset = models.Disk.objects.all()
     table = tables.DiskTable
 
 class DiskSetListView(generic.ObjectListView):
-    template_name = 'netbox_storage_plugin/storage_list.html'
     queryset = models.DiskSet.objects.all()
     table = tables.DiskSetTable
 
 class LogicalDriveListView(generic.ObjectListView):
-    template_name = 'netbox_storage_plugin/storage_list.html'
     queryset = models.LogicalDrive.objects.all()
     table = tables.LogicalDriveTable
 
 class FilesystemListView(generic.ObjectListView):
-    template_name = 'netbox_storage_plugin/storage_list.html'
     queryset = models.Filesystem.objects.all()
     table = tables.FilesystemTable
 
 class ShareListView(generic.ObjectListView):
-    template_name = 'netbox_storage_plugin/storage_list.html'
     queryset = models.Share.objects.all()
     table = tables.ShareTable
 
 class SANVolumeListView(generic.ObjectListView):
-    template_name = 'netbox_storage_plugin/storage_list.html'
     queryset = models.SANVolume.objects.all()
     table = tables.SANVolumeTable
 
 class ObjectStorageListView(generic.ObjectListView):
-    template_name = 'netbox_storage_plugin/storage_list.html'
     queryset = models.ObjectStorage.objects.all()
     table = tables.ObjectStorageTable
 
 class VirtualDiskListView(generic.ObjectListView):
-    template_name = 'netbox_storage_plugin/storage_list.html'
     queryset = models.VirtualDisk.objects.all()
     table = tables.VirtualDiskTable
 
 class DiskEditView(generic.ObjectEditView):
-    template_name = 'netbox_storage_plugin/storage_edit.html'
     queryset = models.Disk.objects.all()
     form = forms.DiskForm
 
 class DiskSetEditView(generic.ObjectEditView):
-    template_name = 'netbox_storage_plugin/storage_edit.html'
     queryset = models.DiskSet.objects.all()
     form = forms.DiskSetForm
 
 class LogicalDriveEditView(generic.ObjectEditView):
-    template_name = 'netbox_storage_plugin/storage_edit.html'
     queryset = models.LogicalDrive.objects.all()
     form = forms.LogicalDriveForm
 
 class FilesystemEditView(generic.ObjectEditView):
-    template_name = 'netbox_storage_plugin/storage_edit.html'
     queryset = models.Filesystem.objects.all()
     form = forms.FilesystemForm
 
 class ShareEditView(generic.ObjectEditView):
-    template_name = 'netbox_storage_plugin/storage_edit.html'
     queryset = models.Share.objects.all()
     form = forms.ShareForm
 
 class SANVolumeEditView(generic.ObjectEditView):
-    template_name = 'netbox_storage_plugin/storage_edit.html'
     queryset = models.SANVolume.objects.all()
     form = forms.SANVolumeForm
 
 class ObjectStorageEditView(generic.ObjectEditView):
-    template_name = 'netbox_storage_plugin/storage_edit.html'
     queryset = models.ObjectStorage.objects.all()
     form = forms.ObjectStorageForm
 
 class VirtualDiskEditView(generic.ObjectEditView):
-    template_name = 'netbox_storage_plugin/storage_edit.html'
     queryset = models.VirtualDisk.objects.all()
     form = forms.VirtualDiskForm
     
