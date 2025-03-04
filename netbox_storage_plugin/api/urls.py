ccd3a1438e550e267e88d5ca24e1ea4cb0c94da5
Eric Hester <eric.hester@umbrella.tech>
2025-03-04 18:57:13 -0500

    .

--- netbox_storage_plugin/api/urls.py
+++ netbox_storage_plugin/api/urls.py
@@ -3,12 +3,12 @@ from . import views
 
 router = NetBoxRouter()
 router.register(r'disks', views.DiskViewSet)
-router.register(r'disk-sets', views.DiskSetViewSet)
-router.register(r'logical-drives', views.LogicalDriveViewSet)
+router.register(r'disksets', views.DiskSetViewSet)
+router.register(r'logicaldrives', views.LogicalDriveViewSet)
 router.register(r'filesystems', views.FilesystemViewSet)
 router.register(r'shares', views.ShareViewSet)
-router.register(r'san-volumes', views.SANVolumeViewSet)
-router.register(r'object-storages', views.ObjectStorageViewSet)
-router.register(r'virtual-disks', views.VMDiskViewSet)
+router.register(r'sanvolumes', views.SANVolumeViewSet)
+router.register(r'objectstorages', views.ObjectStorageViewSet)
+router.register(r'virtualdisks', views.VMDiskViewSet)
 
 urlpatterns = router.urls
